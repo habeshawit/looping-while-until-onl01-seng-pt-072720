607c3a2379d0d327460aeabd9b088397fd3973da
Shalom Kebede <shalomseleshi@gmail.com>
2020-07-31 00:01:34 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,13 @@
+require 'pry'
+
 def using_until
   levitation_force = 6
-  #your code here
-    
+  
+  until levitation_force == 10
+    puts "Wingardium Laviosa"
+    levitation_force +=10
+  end
+  
 end
 
+binding.pry
